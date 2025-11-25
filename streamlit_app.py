6d7f97cb203602fcba2cc6447a9f57bcf3f0442c
arnanbonny <65863796+arnanbonny@users.noreply.github.com>
2025-11-25 22:17:15 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,6 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 y_pred = rf_model.predict(X_test)
 mae =  mean_absolute_error(y_test, y_pred)
 rmse =  mean_squared_error(y_test, y_pred, squared = False)
-r2 r2_score(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
 
 st.write(r2)
