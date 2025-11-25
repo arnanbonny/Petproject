5d5028cebaa2bc22789991bd7a0ddbc29d66b663
arnanbonny <65863796+arnanbonny@users.noreply.github.com>
2025-11-25 22:25:06 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,10 @@ X = df_encoded.drop('price', axis = 1)
 y = df_encoded['price']
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random_state = 42)
 scaler = StandardScaler()
-X_train[num_cols]= scaler.fit_transform(X_train[num_cols])
-X_test[num_cols] = scaler.transform(X_test[num_cols])
+
+X_train.loc[:, num_cols] = scaler.fit_transform(X_train[num_cols])
+X_test.loc[:, num_cols] = scaler.transform(X_test[num_cols])
+
 rf_model = RandomForestRegressor(n_estimators = 100, random_state =42)
 rf_model.fit(X_train, y_train)
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
