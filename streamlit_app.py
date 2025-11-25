23d45a4f1ac65236faf658db3a1baf0aba1c0c90
arnanbonny <65863796+arnanbonny@users.noreply.github.com>
2025-11-25 22:16:59 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,8 @@ rf_model = RandomForestRegressor(n_estimators = 100, random_state =42)
 rf_model.fit(X_train, y_train)
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 y_pred = rf_model.predict(X_test)
-mae =  mean_absolute_error(y_test, y_pred))
-rmse =  mean_squared_error(y_test, y_pred, squared = False))
-r2 r2_score(y_test, y_pred))
+mae =  mean_absolute_error(y_test, y_pred)
+rmse =  mean_squared_error(y_test, y_pred, squared = False)
+r2 r2_score(y_test, y_pred)
 
 st.write(r2)
